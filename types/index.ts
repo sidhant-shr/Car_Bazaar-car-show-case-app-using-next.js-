b7cd6d975591221a1ce40ae72bff578fd5da4da4
Sidhant <shr.sidhant@gmail.com>
2023-06-17 13:47:36 +0530

    search bar completed

--- types/index.ts
+++ types/index.ts
@@ -35,3 +35,14 @@ export interface CarProps{
     year:number;
 
 }
+
+export interface FilterProps{
+    manufacturer: string,
+    year: number,
+    fuel: string,
+    limit: number,
+    model: string,
+
+
+
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps,FilterProps } from "@/types";
 
-export async function fetchCars(){
+
+export async function fetchCars(filters: FilterProps){
+    const {manufacturer, year, model, limit, fuel}= filters;
     const headers = {
         'X-RapidAPI-Key': '4f0688be6bmsh332a6ce66f05f37p13704ejsnc1fc20344749',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers
 
     });
